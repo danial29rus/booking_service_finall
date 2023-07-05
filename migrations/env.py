349443c927fd14b0558a52af6e54476e2c197689
Daniil <70702619+danial29rus@users.noreply.github.com>
2023-07-05 14:09:00 +0300

    dsa

--- migrations/env.py
+++ migrations/env.py
@@ -30,12 +30,6 @@ config.set_section_option(section, "DB_PASS", DB_PASS)
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-# add your model's MetaData object here
-# for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
-target_metadata = Base.metadata
-
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
